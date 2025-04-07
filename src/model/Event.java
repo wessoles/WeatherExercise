abb088ad73fa56128f1f1fb2ab95298624305c5b
nicolas <nicolas_bessoles@hotmail.com>
2025-04-07 15:38:10 +0200

    Add boolean methods for Event

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,11 +1,21 @@
 package model;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class Event {
 
-    private float latitude;
-    private float longitude;
+    private Double latitude;
+    private Double longitude;
     private Instant startTime;
     private Instant endTime;
+
+    public boolean hasLocation() {
+        return latitude != null && longitude != null;
+    }
+
+    public boolean isInTheNext7Days() {
+        Instant now = Instant.now();
+        return !startTime.isBefore(now) && startTime.isBefore(now.plus(7, ChronoUnit.DAYS));
+    }
 }
