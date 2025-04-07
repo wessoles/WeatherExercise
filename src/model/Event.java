549234aac98bedf304310d0fa1da07821071a0c7
nicolas <nicolas_bessoles@hotmail.com>
2025-04-07 15:33:10 +0200

    Add Event class

---
+++ src/model/Event.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.Instant;
+
+public class Event {
+
+    private float latitude;
+    private float longitude;
+    private Instant startTime;
+    private Instant endTime;
+}
