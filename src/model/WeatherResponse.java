90f649cedaa4e9bd08f08bd82ff37ac12be8fad2
nicolas <nicolas_bessoles@hotmail.com>
2025-04-07 15:56:16 +0200

    Add weatherResponse

---
+++ src/model/WeatherResponse.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public record WeatherResponse(double temperature, double windSpeed, Instant fetchedAt) {
+    public boolean isExpired() {
+        return Duration.between(fetchedAt, Instant.now()).toHours() >= 2;
+    }
+}
